a8dc86693cca638cd513d2a77797bc2bec8f68d4
WalkFen24 <133035558+WalkFen24@users.noreply.github.com>
2024-05-15 14:31:14 -0700

    Delete Product/trashbash directory

--- Product/trashbash/src/main/java/com/yearendproject/trashbash/Game.java
+++
@@ -1,4 +0,0 @@
-package com.yearendproject.trashbash;
-
-public class Game {
-}
--- Product/trashbash/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.yearendproject.trashbash {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.yearendproject.trashbash to javafx.fxml;
-    exports com.yearendproject.trashbash;
-}
