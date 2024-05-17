6eb4bfe74802299646bf438ae8a08a64c445fc69
WalkFen24 <f.d.walker@outlook.com>
2024-05-17 13:31:20 -0700

    code update

--- Product/trashbash/src/main/java/com/yearendproject/trashbash/Game.java
+++ Product/trashbash/src/main/java/com/yearendproject/trashbash/Game.java
@@ -1,4 +1,25 @@
 package com.yearendproject.trashbash;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import javax.crypto.spec.PSource;
+
 public class Game {
+
+    @FXML
+    public Rectangle blueBin;
+    @FXML
+    public Rectangle redBin;
+
+    public Game() {
+        System.out.println("game start");
+    }
+
+
 }
--- Product/trashbash/src/main/java/com/yearendproject/trashbash/HelloApplication.java
+++ Product/trashbash/src/main/java/com/yearendproject/trashbash/HelloApplication.java
@@ -1,18 +1,36 @@
 package com.yearendproject.trashbash;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Stage stg;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Trash Bash");
+
+        //used Bro Code's video on "JavaFX KeyEvent"
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                System.out.println(keyEvent.getCode());
+                if (keyEvent.getCode() == KeyCode.A) {
+
+                }
+            }
+        });
+
         stage.setScene(scene);
         stage.show();
     }
